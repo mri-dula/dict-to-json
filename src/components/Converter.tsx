acdb977048360b2d390ceb878de2dd26663457d6
Mridula Prabhu <mridulaprabhu@Mridulas-MacBook-Pro.local>
2023-12-31 10:02:45 +0530

    refactor: minor improvements

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -5,13 +5,13 @@
 import { CardHeader, CardContent, Card } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
-import { Avatar } from "@/components/ui/avatar"
-
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import avatar from '../assets/avatar.png';
 
 export default function Converter() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
-      <Card className="w-full max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+      <Card className="w-full mx-auto bg-white shadow-md rounded-lg overflow-hidden">
         <CardHeader className="flex justify-between items-center p-12">
           <h2 className="text-2xl font-semibold">Dict to JSON Converter</h2>
         </CardHeader>
@@ -20,7 +20,7 @@ export default function Converter() {
             <div>
               <div className="font-semibold">Python Dictionary</div>
               <Textarea
-                className="w-full h-24 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-white p-4"
+                className="w-full h-64 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-white p-4"
                 id="python-dict"
                 placeholder="Enter Python dictionary here"
               />
@@ -28,24 +28,24 @@ export default function Converter() {
             <div>
               <div className="font-semibold">JSON</div>
               <Textarea
-                className="w-full h-24 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-white p-4"
+                className="w-full h-64 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-white p-4"
                 id="json-output"
                 placeholder="Converted JSON will appear here"
               />
             </div>
           </div>
           <div className="flex justify-center mt-4">
-            <Button className="bg-blue-500 text-white rounded-md px-4 py-2">Convert</Button>
+            <Button className="bg-blue-500 text-white rounded-md px-4 py-2 mr-2">Convert + Minify</Button>
+            <Button className="bg-blue-500 text-white rounded-md px-4 py-2">Convert + Beautify</Button>
           </div>
         </CardContent>
       </Card>
       <div className="mt-4 flex items-center">
-        <Avatar className="w-10 h-10 rounded-full" src="/placeholder.svg?height=50&width=50" />
+        <Avatar className="w-10 h-10 rounded-full">
+            <AvatarImage src={avatar} />
+        </Avatar>
         <div className="ml-2">
           <div className="text-sm font-medium">Mridula Prabhu</div>
-          <Button className="text-xs text-blue-500" >
-            View Source Code
-          </Button>
         </div>
       </div>
     </main>
